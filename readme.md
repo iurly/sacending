287ee043c5650d1bb1362ece06293bf417e51b28
Gerlando Falauto <gerlando.falauto@st.com>
2020-02-10 21:31:22 +0100

    initial

---
+++ readme.md
@@ -0,0 +1,2 @@
+http://raspbian.raspberrypi.org/raspbian/pool/main/f/fonts-dseg/
+http://raspbian.raspberrypi.org/raspbian/pool/main/f/fonts-dseg/fonts-dseg_0.45-1_all.deb
---
+++ sacending.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python
+
+import sys
+from tkinter import *
+from tkinter import ttk
+from tkinter import font
+import time
+import datetime
+
+global endTime
+
+def quit(*args):
+    root.destroy()
+
+def show_time():
+    # Get the time remaining until the event
+    remainder = endTime - datetime.datetime.now()
+    # remove the microseconds part
+    remainder = remainder - datetime.timedelta(microseconds=remainder.microseconds)
+    # Show the time left
+    txt.set(remainder)
+    # Trigger the countdown after 1000ms
+    root.after(1000, show_time)
+
+# Use tkinter lib for showing the clock
+root = Tk()
+root.attributes("-fullscreen", True)
+root.configure(background='black')
+root.bind("x", quit)
+root.after(1000, show_time)
+
+# Set the end date and time for the countdown
+endTime = datetime.datetime(2017, 9, 19, 9, 0, 0)
+
+#fnt = font.Font(family='DSEG7-Classic', size=60, weight='bold')
+fnt = font.Font(family='DSEG7 Classic')
+print(font.families())
+
+#sys.exit(1)
+txt = StringVar()
+lbl = ttk.Label(root, textvariable=txt, font=fnt, foreground="green", background="black")
+lbl.place(relx=0.5, rely=0.5, anchor=CENTER)
+
+root.mainloop()
